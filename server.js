17e5110507eead96c250b3e31a13b7402f79653c
Asil <asilefeakyol@hotmail.com>
2020-09-13 23:25:37 +0300

    better ui

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let results = [];
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
